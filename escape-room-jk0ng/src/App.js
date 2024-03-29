7fd2937b5326d1e159526e020e44839de87d36aa
Joseph Fansen Kong <joseph.kong@planetargon.com>
2024-03-29 09:30:24 -0700

    created useReducer for state management

--- escape-room-jk0ng/src/App.js
+++ escape-room-jk0ng/src/App.js
@@ -1,23 +1,28 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 import './App.css'
+import { reducer } from './reducer/reducer'
 import Secret from './components/Secret/Secret'
 import Rows from './components/Rows/Rows'
 import Buttons from './components/Buttons/Buttons'
 import Colors from './components/Colors/Colors'
 import GameEnds from './components/GameEnds/GameEnds'
-
+import AppContext from './context/Contexts'
 
 const App = () => {
+    const [appState, dispatch] = useReducer(reducer, { test: true })
+
     return (
-        <div className="App">
-            <div className="board">
-                <Rows/> 
-                <Colors/>
-                <Buttons/>
-                <Secret/>
-                {/* <GameEnds/> */}
+        <AppContext.Provider value={[appState, dispatch]}>
+            <div className="App">
+                <div className="board">
+                    <Rows />
+                    <Colors />
+                    <Buttons />
+                    <Secret />
+                    {/* <GameEnds/> */}
+                </div>
             </div>
-        </div>
+        </AppContext.Provider>
     );
 }
 export default App
